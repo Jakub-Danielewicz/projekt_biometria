3e64878a1d9076335688c0ac4dc40c83ea32c9a1
Akinott <99678974+Akinott@users.noreply.github.com>
2025-06-01 18:01:29 +0200

    model save & load

--- python_model/train.py
+++ python_model/train.py
@@ -6,57 +6,46 @@ from models.cnn import HandwritingCNN, HandwritingOCRNet
 from models.mlp import  HandwritingMLP
 
 from data import scan_image_folder, OCRDataset, OCRDataLoader
-from data.transforms import Compose, Resize, Threshold, ToTensor, Erode, Invert, RandomShift, RandomRotate
+from data.transforms import Compose, Resize, Threshold, ToTensor, Erode, Invert
 
-def validateModel(model, dataloader):   
-    global device, setoflabels, dataset
-    dataset.transform = val_transform
+#from save_model import save_checkpoint, load_checkpoint, generate_checkpoint_name
+
+def validateModel(model, dataloader):
+    global device
     model.eval()
     correct = 0
     total = 0
     total_loss = 0
     with torch.no_grad():
         for images, labels in dataloader:
-            if len(images) == 0:
-                continue
             images, labels = images.to(device), labels.to(device)
             outputs = model(images)
             _, preds = torch.max(outputs, 1)
-            #for p, l in zip(preds.tolist(), labels.tolist()):
-            #    print(f"Predykcja: {setoflabels[p]}, label: {setoflabels[l]}")
             correct += (preds == labels).sum().item()
             total += labels.size(0)
     return correct/total
 
 VAL_EVERY = 3
-BATCH_SIZE =  32
-LR = 0.001
-NUM_EPOCHS = 50
-DATA_DIR = "./python_model/data/set/output_letters_cleaned"
+BATCH_SIZE = 18
+LR = 0.01
+DATA_DIR = ".\python_model\data\output_letters_cleaned"
 
 if __name__ == "__main__":
     # Transform pipeline
-    train_transform = Compose([
-    Resize((64, 64)),
-    RandomRotate(angle=10),
-    RandomShift(max_shift=4),
-    ToTensor()
-    ])
-
-    val_transform = Compose([
+    transform = Compose([
         Resize((64, 64)),
+        Threshold(190),
+        Erode(),
         ToTensor()
     ])
 
 
     image_paths, labels = scan_image_folder(DATA_DIR)
-    setoflabels = sorted(set(labels))
     class_to_idx = {cls: idx for idx, cls in enumerate(sorted(set(labels)))}
     labels = [class_to_idx[l] for l in labels]
 
-    print(image_paths[0], labels[0])
-    dataset = OCRDataset(image_paths, labels, transform=train_transform)
-    #dataset.apply_transform()
+    dataset = OCRDataset(image_paths, labels, transform=transform)
+    dataset.apply_transform()
 
     # Tworzenie dataloadera z podziałem na walidację
     dataloader = OCRDataLoader(dataset, batch_size=BATCH_SIZE, validation_split=0.2, shuffle=True)
@@ -70,14 +59,13 @@ if __name__ == "__main__":
     if torch.cuda.is_available():
         print("Trenowanie na CUDA")
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model = HandwritingOCRNet(num_classes=len(set(labels))).to(device)
+    model = HandwritingMLP(num_classes=len(set(labels))).to(device)
 
     loss_fn = nn.CrossEntropyLoss()
     optimizer = optim.Adam(model.parameters(), lr=LR)
 
     # --- Trening ---
-    for epoch in range(NUM_EPOCHS):
-        dataset.transform = train_transform
+    for epoch in range(3):
         model.train()
         total_loss = 0
         i = 0
@@ -86,8 +74,6 @@ if __name__ == "__main__":
            # print("images:", images.shape, images.min().item(), images.max().item())
            # print("labels:", labels[:10])
             #break
-            if len(images) == 0:
-                continue
             images, labels = images.to(device), labels.to(device)
             optimizer.zero_grad()
             outputs = model(images)
@@ -103,4 +89,16 @@ if __name__ == "__main__":
         if (epoch+1)%VAL_EVERY == 0:
             print(f"Dokładność na zbiorze walidacyjnym: {validateModel(model, val_loader)*100}%")
 
+    checkpoint = {
+        'epoch': epoch,
+        'model_state_dict': model.state_dict(),
+        'optimizer_state_dict': optimizer.state_dict(),
+        'loss': loss
+    }
 
+    # Nazwa pliku np. na podstawie parametrów
+    filename = f"checkpoint_epoch{epoch}_loss{total_loss:.4f}.pt"
+
+    # Zapis do pliku
+    torch.save(checkpoint, filename)
+    print(f"Zapisano model do {filename}")
