da904c99035ea5082b2068279950dda6ea658205
Jakub Danielewicz <kub3100@gmail.com>
2025-06-26 15:34:11 +0200

    usunięcie zbędnych komentarzy

--- python_model/train.py
+++ python_model/train.py
@@ -1,4 +1,3 @@
-#To jest wzięte żywcem z chata jkc z minimalnymi modyfikacjami
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -35,8 +34,6 @@ def validateModel(model, dataloader):
             images, labels = images.to(device), labels.to(device)
             outputs = model(images)
             _, preds = torch.max(outputs, 1)
-           # for p, l in zip(preds.tolist(), labels.tolist()):
-            #    print(f"Predykcja: {setoflabels[p]}, label: {setoflabels[l]}")
             correct += (preds == labels).sum().item()
             total += labels.size(0)
     return correct/total
@@ -63,14 +60,11 @@ if __name__ == "__main__":
 
     print(image_paths[0], labels[0])
     dataset = OCRDataset(image_paths, labels, transform=train_transform)
-    #dataset.apply_transform()
 
-    # Tworzenie dataloadera z podziałem na walidację
     dataloader = OCRDataLoader(dataset, batch_size=BATCH_SIZE, validation_split=0.2, shuffle=True)
-    train_loader = dataloader  # to jest OK, bo OCRDataLoader dziedziczy po DataLoader
-    val_loader = dataloader.split_validation()  # to jest DataLoader z samplerem walidacyjnym
+    train_loader = dataloader 
+    val_loader = dataloader.split_validation()  
 
-    # Sprawdź, czy val_loader nie jest None
     if val_loader is None:
         raise ValueError("Validation loader is None! Sprawdź parametr validation_split.")
 
@@ -89,7 +83,6 @@ if __name__ == "__main__":
     loss_fn = nn.CrossEntropyLoss()
     optimizer = optim.Adam(model.parameters(), lr=LR)
 
-    # --- Trening ---
     for epoch in range(NUM_EPOCHS):
         dataset.transform = train_transform
         model.train()
@@ -97,9 +90,6 @@ if __name__ == "__main__":
         i = 0
         correct = 0
         for images, labels in train_loader:
-           # print("images:", images.shape, images.min().item(), images.max().item())
-           # print("labels:", labels[:10])
-            #break
             if len(images) == 0:
                 continue
             images, labels = images.to(device), labels.to(device)
@@ -117,8 +107,6 @@ if __name__ == "__main__":
         if (epoch+1)%VAL_EVERY == 0:
             print(f"Dokładność na zbiorze walidacyjnym: {validateModel(model, val_loader)*100}%")
 
-
-    #zapis checkpointu - nazwa generuje się na podstawie epoch i loss lub można podać własną
     setattr(model, '_kwargs', {'num_classes': len(setoflabels)})
     save_checkpoint(model, optimizer, epoch, total_loss, LR, "checkp_2.pt")
 
--- python_model/visualize_model.py
+++ python_model/visualize_model.py
@@ -11,15 +11,15 @@ load_dotenv()
 MODEL = os.getenv("MODEL")
 BATCH_SIZE = int(os.getenv("BATCH_SIZE"))
 DATA_DIR = os.getenv("DATA_DIR")
-CHECKPOINT_PATH = os.getenv("CHECKPOINT_PATH", "checkpoint.pt")  # Możesz ustawić ścieżkę do checkpointa w .env
+CHECKPOINT_PATH = os.getenv("CHECKPOINT_PATH", "checkpoint.pt")  #
 
-# Przygotuj transformacje walidacyjne
+
 val_transform = Compose([
     Resize((64, 64)),
     ToTensor()
 ])
 
-# Przygotuj dane i dataloader
+
 image_paths, labels = scan_image_folder(DATA_DIR)
 setoflabels = sorted(set(labels))
 class_to_idx = {cls: idx for idx, cls in enumerate(setoflabels)}
@@ -27,12 +27,12 @@ labels = [class_to_idx[l] for l in labels]
 dataset = OCRDataset(image_paths, labels, transform=val_transform)
 dataloader = OCRDataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True)
 
-# Ustal urządzenie
+
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-# Załaduj model i optimizer
+
 model, optimizer, start_epoch, loss = load_checkpoint(CHECKPOINT_PATH)
 model = model.to(device)
 
-# Wizualizacja predykcji
+
 visualize_predictions(model, dataloader, class_names=setoflabels, device=device, num_images=6)
